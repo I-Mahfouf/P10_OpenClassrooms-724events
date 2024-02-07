f39ba4c7fd1d5fe52f4f04e42b5b5ca69b8908a1
I-Mahfouf <143210485+I-Mahfouf@users.noreply.github.com>
2024-02-07 18:49:11 +0100

    Résolution du filtrage
    - Résolution du problème de filtrage de la section "Nos réalisations".
    Il est maintenant possible de filtrer les événements selon le filtre sélectionné.
    - Ajout d'un curseur sur la flèche pour indiquer à l'utilisateur qu'il est possible de cliquer sur l'élément.

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -16,7 +16,7 @@ const EventList = () => {
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
+      : data?.events.filter((event) => event.type === type))  || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
